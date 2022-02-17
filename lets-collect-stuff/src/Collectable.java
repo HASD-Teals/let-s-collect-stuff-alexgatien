4944de2ef3dc47a7f7f8f088b46f3a646f0959e1
alexgatien <alexgatien14@gmail.com>
2022-02-17 09:59:51 -0600

    Start of Collectable

--- lets-collect-stuff/src/Collectable.java
+++ lets-collect-stuff/src/Collectable.java
@@ -1,8 +1,18 @@
 public class Collectable {
     // Add collectable properties here
+private String albumName;
+private String artistName;
+private int levelofDamage;
+private double value;
 
     // Add collectable constructors here
-
+public Collectable(){}
+public Collectable(String albumName, String artistName, int levelofDamage, double value){
+this.albumName = albumName;
+this.artistName = artistName;
+this.levelofDamage = levelofDamage;
+this.value = value;
+}
     // Add collectable accessors and mutators here
 
     // Add any methods here
